3fa0939608aa0ad8d7eac4f0c632af4dac4e998c
StellarWasp-ian <avilaian2officialac@gmail.com>
2026-03-18 17:10:05 -0300

    Se inicio la creacion de la clase pizarra

---
+++ TP1_HERENCIA/EJER9/pizarra.java
@@ -0,0 +1,26 @@
+package TP1_HERENCIA.EJER9;
+
+public class pizarra {
+    
+    private String colorfondo;
+
+    public pizarra(String colorfondo) {
+        this.colorfondo = colorfondo;
+    }
+
+    public String getColorfondo() {
+        return colorfondo;
+    }
+
+    public void setColorfondo(String colorfondo) {
+        this.colorfondo = colorfondo;
+    }
+     
+    public void agregarFigura(){
+
+    }
+    public void eliminarFigura(){
+        
+    }
+
+}

749ca8295d739855b80a7d3b88c0e11b98ff1f41
StellarWasp-ian <avilaian2officialac@gmail.com>
2026-03-18 16:29:16 -0300

    Iniciado el TP1 - Punto 1, Se creo la clase abstracta Figura

---
+++ TP1_HERENCIA/EJER9/figura.java
@@ -0,0 +1,30 @@
+package TP1_HERENCIA.EJER9;
+
+abstract public class figura {
+    private String colorfondo;
+    private String colorborde;
+
+    //Constructor
+    public figura(String colorfondo, String colorborde) {
+        this.colorfondo = colorfondo;
+        this.colorborde = colorborde;
+    }
+    //Gets y Sets
+    public String getColorfondo() {
+        return colorfondo;
+    }
+    public void setColorfondo(String colorfondo) {
+        this.colorfondo = colorfondo;
+    }
+    public String getColorborde() {
+        return colorborde;
+    }
+    public void setColorborde(String colorborde) {
+        this.colorborde = colorborde;
+    }
+
+    public abstract void calcSuperficie();
+    public abstract void calcPerimetro();
+    public abstract String dibujarFigura();
+    
+}
---
+++ TP1_HERENCIA/EJER9/rectangulo.java
@@ -0,0 +1,24 @@
+package TP1_HERENCIA.EJER9;
+
+import java.io.Console;
+
+public class rectangulo extends figura{
+
+    private double base;
+    private double altura;
+    
+
+    @Override
+    public void calcSuperficie() {
+    }
+
+    @Override
+    public void calcPerimetro() {
+    }
+
+    @Override
+    public String dibujarFigura() {
+        return rectangulo;
+    }
+    
+}

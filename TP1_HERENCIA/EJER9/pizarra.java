f2c0c436e1ca304709c5af4b0dc4acdaada776dd
StellarWasp-ian <avilaian2officialac@gmail.com>
2026-03-18 17:20:46 -0300

    Se agrego el vector de figuras (donde se almacenan los objetos figura)

--- TP1_HERENCIA/EJER9/pizarra.java
+++ TP1_HERENCIA/EJER9/pizarra.java
@@ -1,5 +1,6 @@
 package TP1_HERENCIA.EJER9;
 
+import java.util.*;
 public class pizarra {
     
     private String colorfondo;
@@ -15,12 +16,18 @@ public class pizarra {
     public void setColorfondo(String colorfondo) {
         this.colorfondo = colorfondo;
     }
-     
+    
+    Vector <figura> figuras = new Vector<figura>();
+
     public void agregarFigura(){
 
     }
     public void eliminarFigura(){
-        
+
+    }
+
+    public void limpiarPizarra(){
+        figuras.clear();
     }
 
 }
